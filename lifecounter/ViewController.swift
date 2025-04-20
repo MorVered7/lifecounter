b137289fc39efde154fde8a66e1a77c676b3740b
MorVered7 <mvered9@gmail.com>
2025-04-19 21:52:07 -0700

    player 1 buttons work

--- lifecounter/ViewController.swift
+++ lifecounter/ViewController.swift
@@ -11,9 +11,29 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        p1_lifecount.text = "\(p1_lifecount_var)"
         // Do any additional setup after loading the view.
     }
-
-
+    @IBOutlet weak var p1_lifecount: UILabel!
+    var p1_lifecount_var: Int = 20
+    
+    @IBOutlet weak var lifecount_player1: UILabel!
+    
+    @IBAction func p1_minus(_ sender: Any) {
+        p1_lifecount_var -= 1
+        p1_lifecount.text = "\(p1_lifecount_var)"
+    }
+    @IBAction func p1_plus(_ sender: Any) {
+        p1_lifecount_var += 1
+        p1_lifecount.text = "\(p1_lifecount_var)"
+    }
+    @IBAction func p1_plus5(_ sender: Any) {
+        p1_lifecount_var += 5
+        p1_lifecount.text = "\(p1_lifecount_var)"
+    }
+    @IBAction func p1_minus5(_ sender: Any) {
+        p1_lifecount_var -= 5
+        p1_lifecount.text = "\(p1_lifecount_var)"
+    }
 }
 
