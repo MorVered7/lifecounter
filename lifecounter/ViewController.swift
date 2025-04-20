51ae83f08679b0988a3f303140f88f228fc1ad57
MorVered7 <mvered9@gmail.com>
2025-04-19 21:58:53 -0700

    buttons have functionality

--- lifecounter/ViewController.swift
+++ lifecounter/ViewController.swift
@@ -12,10 +12,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         p1_lifecount.text = "\(p1_lifecount_var)"
+        lifecount_player2.text = "\(p2_lifecount_var)"
         // Do any additional setup after loading the view.
     }
     @IBOutlet weak var p1_lifecount: UILabel!
     var p1_lifecount_var: Int = 20
+    @IBOutlet weak var lifecount_player2: UILabel!
+    var p2_lifecount_var: Int = 20
     
     @IBOutlet weak var lifecount_player1: UILabel!
     
@@ -34,6 +37,22 @@ class ViewController: UIViewController {
     @IBAction func p1_minus5(_ sender: Any) {
         p1_lifecount_var -= 5
         p1_lifecount.text = "\(p1_lifecount_var)"
+    }
+    @IBAction func p2_minus(_ sender: Any) {
+        p2_lifecount_var -= 1
+        lifecount_player2.text = "\(p2_lifecount_var)"
+    }
+    @IBAction func p2_plus(_ sender: Any) {
+        p2_lifecount_var += 1
+        lifecount_player2.text = "\(p2_lifecount_var)"
+    }
+    @IBAction func p2_minus5(_ sender: Any) {
+        p2_lifecount_var -= 5
+        lifecount_player2.text = "\(p2_lifecount_var)"
+    }
+    @IBAction func p2_plus5(_ sender: Any) {
+        p2_lifecount_var += 5
+        lifecount_player2.text = "\(p2_lifecount_var)"
     }
 }
 
