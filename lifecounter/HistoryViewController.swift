de2181a3269e548e84e34298235a83e7b8945b38
MorVered7 <mvered9@gmail.com>
2025-04-22 22:41:44 -0700

    did v2

---
+++ lifecounter/HistoryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HistoryViewController.swift
+//  lifecounter
+//
+//  Created by Mor Vered on 4/22/25.
+//
+
+import UIKit
+
+class HistoryViewController: UIViewController {
+    var history: [String] = []
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        historyTextView.text = history.joined(separator: "\n")
+        // Do any additional setup after loading the view.
+    }
+    @IBOutlet weak var historyTextView: UITextView!
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- lifecounter/ViewController.swift
+++ lifecounter/ViewController.swift
@@ -9,69 +9,119 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    struct Player {
+        var life: Int
+    }
+    var history: [String] = []
+
+    var players: [Player] = []
+    var gameStarted = false
+    @IBAction func addPlayer(_ sender: Any) {
+        guard players.count < 8 else { return }
+        addPlayerView(index: players.count)
+    }
+    @IBOutlet weak var addPlayerButton: UIButton!
+    
+    @IBOutlet weak var loseButton: UILabel!
+    @IBOutlet weak var stackview: UIStackView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        p1_lifecount.text = "\(p1_lifecount_var)"
-        lifecount_player2.text = "\(p2_lifecount_var)"
-        // Do any additional setup after loading the view.
-        lose.isHidden = true
-    }
-    @IBOutlet weak var p1_lifecount: UILabel!
-    var p1_lifecount_var: Int = 20
-    @IBOutlet weak var lifecount_player2: UILabel!
-    var p2_lifecount_var: Int = 20
-    
-    @IBOutlet weak var lose: UITextField!
-    
-    @IBOutlet weak var lifecount_player1: UILabel!
-    
-    @IBAction func p1_minus(_ sender: Any) {
-        p1_lifecount_var -= 1
-        p1_lifecount.text = "\(p1_lifecount_var)"
-        if p1_lifecount_var < 1 {
-            lose.text = "PLAYER 1 LOSES!"
-            lose.isHidden = false
+        loseButton.isHidden = true
+
+        for i in 0..<2 {
+            addPlayerView(index: i)
         }
     }
-    @IBAction func p1_plus(_ sender: Any) {
-        p1_lifecount_var += 1
-        p1_lifecount.text = "\(p1_lifecount_var)"
+    func addPlayerView(index: Int) {
+        players.append(Player(life: 20))
+        let playerView = createPlayerView(for: index)
+        stackview.addArrangedSubview(playerView)
     }
-    @IBAction func p1_plus5(_ sender: Any) {
-        p1_lifecount_var += 5
-        p1_lifecount.text = "\(p1_lifecount_var)"
+    func createPlayerView(for index: Int) -> UIView {
+        
+        let player = UIStackView()
+        player.axis = .horizontal
+        player.heightAnchor.constraint(equalToConstant: 40).isActive = true
+
+        let label = UILabel()
+        label.text = "Player \(index + 1): 20"
+        label.tag = 100 + index
+
+        player.addArrangedSubview(label)
+        let input = UITextField()
+        input.placeholder = "1"
+        input.tag = 300 + index
+        input.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        input.keyboardType = .numberPad
+
+        player.addArrangedSubview(input)
+
+        let minus = UIButton(type: .system)
+        minus.setTitle("-", for: .normal)
+        minus.tag = index
+        minus.addTarget(self, action: #selector(decreaseLife(_:)), for: .touchUpInside)
+
+        player.addArrangedSubview(minus)
+
+        let plus = UIButton(type: .system)
+        plus.setTitle("+", for: .normal)
+        plus.tag = index
+        plus.addTarget(self, action: #selector(increaseLife(_:)), for: .touchUpInside)
+
+        player.addArrangedSubview(plus)
+        
+
+        return player
+
     }
-    @IBAction func p1_minus5(_ sender: Any) {
-        p1_lifecount_var -= 5
-        p1_lifecount.text = "\(p1_lifecount_var)"
-        if p1_lifecount_var < 1 {
-            lose.text = "PLAYER 1 LOSES!"
-            lose.isHidden = false
+    @objc func increaseLife(_ sender: UIButton) {
+        let index = sender.tag
+        guard let inputField = stackview.viewWithTag(300 + index) as? UITextField else {return}
+        let amountText = inputField.text ?? ""
+        let amount = Int(amountText) ?? 1
+        
+        players[index].life += amount
+        updatePlayerLabel(index: index)
+        gameStarted = true
+        addPlayerButton.isEnabled = false
+        checkLoss(index: index)
+        history.append("Player \(index + 1) gained \(amount) life.")
+
+    }
+
+    @objc func decreaseLife(_ sender: UIButton) {
+        let index = sender.tag
+        guard let inputField = stackview.viewWithTag(300 + index) as? UITextField else {return}
+        let amountText = inputField.text ?? ""
+        let amount = Int(amountText) ?? 1
+
+        players[index].life -= amount
+        updatePlayerLabel(index: index)
+        gameStarted = true
+        addPlayerButton.isEnabled = false
+        checkLoss(index: index)
+        history.append("Player \(index + 1) lost \(amount) life.")
+
+    }
+    func updatePlayerLabel(index: Int) {
+        if let label = stackview.viewWithTag(100 + index) as? UILabel {
+            label.text = "Player \(index + 1): \(players[index].life)"
         }
     }
-    @IBAction func p2_minus(_ sender: Any) {
-        p2_lifecount_var -= 1
-        lifecount_player2.text = "\(p2_lifecount_var)"
-        if p2_lifecount_var < 1 {
-            lose.text = "PLAYER 2 LOSES!"
-            lose.isHidden = false
+    func checkLoss(index: Int) {
+        if players[index].life < 1 {
+            loseButton.isHidden = false
+            loseButton.text = "PLAYER \(index + 1) LOSES!"
+            history.append("Player \(index + 1) lost.")
         }
     }
-    @IBAction func p2_plus(_ sender: Any) {
-        p2_lifecount_var += 1
-        lifecount_player2.text = "\(p2_lifecount_var)"
-    }
-    @IBAction func p2_minus5(_ sender: Any) {
-        p2_lifecount_var -= 5
-        lifecount_player2.text = "\(p2_lifecount_var)"
-        if p2_lifecount_var < 1 {
-            lose.text = "PLAYER 2 LOSES!"
-            lose.isHidden = false
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showHistory" {
+            let destination = segue.destination as? HistoryViewController
+            destination?.history = history
         }
     }
-    @IBAction func p2_plus5(_ sender: Any) {
-        p2_lifecount_var += 5
-        lifecount_player2.text = "\(p2_lifecount_var)"
-    }
+
+
 }
 
