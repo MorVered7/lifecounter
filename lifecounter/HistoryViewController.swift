5880e0aecdcdbfb27935ff6e58486812b3eb254b
MorVered7 <mvered9@gmail.com>
2025-04-23 18:30:44 -0700

    done with back button

--- lifecounter/HistoryViewController.swift
+++ lifecounter/HistoryViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class HistoryViewController: UIViewController {
     var history: [String] = []
+    
+    @IBAction func backButtonPressed(_ sender: Any) {
+        self.dismiss(animated: true)
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         historyTextView.text = history.joined(separator: "\n")
